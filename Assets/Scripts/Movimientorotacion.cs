349544380a3f2a9cff3c5a657e578f93f104441f
Sacc03 <santiago.cardozo01@usa.edu.co>
2024-03-05 09:11:47 -0500

    Clase 05_03
    Movimiento y rotación personaje

---
+++ Assets/Scripts/Movimientorotacion.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimientorotacion : MonoBehaviour
+{
+    public Vector3 vectorRotacion;
+    public float speed;
+    private new Rigidbody rigidbody;
+    public float movementSpeed;
+    Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        vectorRotacion = this.transform.localEulerAngles;
+        rigidbody = GetComponent<Rigidbody>();
+        
+    }
+
+    void Update()
+    { 
+        
+        if(this.tag=="player1")
+        { 
+            
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                
+                 Vector3 direction = (transform.forward * 1).normalized;
+                    rigidbody.velocity = direction * movementSpeed;
+            }
+             else
+            {
+            rigidbody.velocity = Vector3.zero;
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                Vector3 direction = (transform.forward * -1).normalized;
+                    rigidbody.velocity = direction * movementSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                vectorRotacion =new Vector3(0,speed*Time.deltaTime,0);
+                //this.transform.localEulerAngles = vectorRotacion;
+                this.transform.Rotate(vectorRotacion,Space.Self);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                vectorRotacion =- new Vector3(0,speed*Time.deltaTime,0);
+                //this.transform.localEulerAngles = vectorRotacion;
+                this.transform.Rotate(vectorRotacion,Space.Self);
+            }
+        }
+
+        if(this.tag=="player2")
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                vectorRotacion =new Vector3(0,speed*Time.deltaTime,0);
+                //this.transform.localEulerAngles = vectorRotacion;
+                this.transform.Rotate(vectorRotacion,Space.Self);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                vectorRotacion = -new Vector3(0,speed*Time.deltaTime,0);
+                //this.transform.localEulerAngles = vectorRotacion;
+                this.transform.Rotate(vectorRotacion,Space.Self);
+            }
+        }
+       
+       
+    }
+}
+
